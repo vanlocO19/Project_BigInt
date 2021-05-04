39ae6dc9e3abf30b0c14789ecb40c72f2b2be7b2
vanloc1808 <vanloc1808@users.noreply.github.com>
2021-05-04 18:13:59 +0700

    assign, add

--- BigInt.h
+++ BigInt.h
@@ -1,3 +1,4 @@
 #pragma  once
 #include "BigInt_core.h"
 
+void freeBI(BI& x);
--- BigInt_operator.cpp
+++ BigInt_operator.cpp
@@ -3,10 +3,32 @@
 #include "BigInt_function.h"
 #include "BigInt.h"
 
-int numberOfDigits(BI x) {
-	
+void assignOperator(BI src, BI& dest) {
+	freeBI(dest);
+	dest.nBytes = src.nBytes;
+	dest.data = (BYTE*)calloc(src.nBytes, sizeof(BYTE));
+	memcpy(dest.data, src.data, src.nBytes);
 }
 
-int getIthDigit(BI x, int i) {
-
+BI addOperator(BI term1, BI term2) {
+	int n;
+	if (term1.nBytes > term2.nBytes) {
+		n = term1.nBytes;
+	}
+	else {
+		n = term2.nBytes;
+	}
+	BYTE* tempData = (BYTE*)calloc(n + 1, sizeof(BYTE)); //should we check NULL pointer?...
+	int tempRes, carry = 0;
+	for (int i = 0; i < n; i++) {
+		tempRes = term1.data[i] + term2.data[i] + carry;
+		tempData[i] = tempRes % 256;
+		carry = tempRes / 256;
+	}
+	if (carry != 0) {
+		tempData[n] = carry;
+		n++;
+	}
+	BI res = { n,tempData };
+	return res;
 }
