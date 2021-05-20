44ab56d7aa7ddbf7dc31c28660b181ecbd7734e4
vanloc1808 <76842571+vanloc1808@users.noreply.github.com>
2021-05-20 22:11:16 +0700

    Update BigInt_Processing.h

--- BigInt_Processing.h
+++ BigInt_Processing.h
@@ -1,8 +1,5 @@
-#pragma once
-#include "BigInt.h"
 #include "BigInt_core.h"
-#include "BigInt_function.h"
-#include "BigInt_operator.h"
+
 
 BI processWhenNIsFour(char* parameter[], BI para1, BI para2, FILE* fo);
 void BIProcess(int n, char* parameter[], FILE* fo);
